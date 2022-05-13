114031ec23dc3a07b4c18cf98714e33f56c375e4
Shrish Sharma <shrish.sharma@ril.com>
2022-05-13 14:59:11 +0530

    Basic variables in GO

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 }
